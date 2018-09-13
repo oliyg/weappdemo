90c9a0e037c4d5983330fb2aceccb33bd858d4d0
oliyg <billyangg@qq.com>
2018-09-13 16:29:59 +0800

    [add] 修改 models 配置文件

--- models/index.js
+++ models/index.js
@@ -5,7 +5,13 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/..\config\config.json')[env];
+const configs = require(__dirname + '/..\config\config.js')[env];
+const config = {
+  ...configs[env],
+  define: {
+    underscored: true,
+  },
+};
 const db = {};
 
 let sequelize;
