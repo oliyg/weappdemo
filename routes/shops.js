613e2b8022fd35d612e86543e004f594e0067134
oliyg <billyangg@qq.com>
2018-09-17 11:29:32 +0800

    [add] 分页逻辑 hapi-pagination

--- routes/shops.js
+++ routes/shops.js
@@ -1,17 +1,31 @@
 const Joi = require('joi')
 const GROUP_NAME = 'shops'
 const models = require('../models')
+const { paginationDefine } = require('../utils/router-helper')
 
 module.exports = [{
     method: 'GET',
     path: `/${GROUP_NAME}`,
     handler: async (request, reply) => {
-        const res = await models.shops.findAll()
-        reply(res)
+        const res = await models.shops.findAndCountAll({
+            attributes: [
+                'id', 'name'
+            ],
+            limit: request.query.limit,
+            offset: (request.query.page - 1) * request.query.limit
+        })
+        const { rows: results, count: totalCount } = res
+        // 开启分页的插件，返回的数据结构里，需要带上 result 与 totalCount 两个字段
+        reply({ results, totalCount })
     },
     config: {
         tags: ['api', GROUP_NAME],
-        description: 'get shop list'
+        description: 'get shop list',
+        validate: {
+            query: {
+                ...paginationDefine
+            }
+        }
     }
 }, {
     method: 'GET',
---
+++ utils/router-helper.js
@@ -0,0 +1,11 @@
+const Joi = require('joi')
+
+const paginationDefine = {
+    limit: Joi.number().integer().min(1).default(10).description('每页条数'),
+    page: Joi.number().integer().min(1).default(1).description('页码数'),
+    pagination: Joi.boolean().description('是否开启分页')
+}
+
+module.exports = {
+    paginationDefine
+}
